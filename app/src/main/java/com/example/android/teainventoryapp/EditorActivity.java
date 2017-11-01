4a138c8ada2d6f56f4fb1f026453e8cfa5d00ef6
Sabina Ioana <sabinaleitoiu91@gmail.com>
2017-11-01 23:11:30 +0000

    New commit

--- app/src/main/java/com/example/android/teainventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/android/teainventoryapp/EditorActivity.java
@@ -1,7 +1,6 @@
 package com.example.android.teainventoryapp;
 
 import android.app.Activity;
-import android.support.v7.app.AlertDialog;
 import android.app.LoaderManager;
 import android.content.ContentResolver;
 import android.content.ContentValues;
@@ -16,6 +15,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
